2769646fde9879803448f5dc8d34dc03dc1c9ed5
Damon McNeill <mcneillbarra@gmail.com>
2021-11-21 12:03:09 -0800

    Add streams to the stdlib

--- samples/streams.s
+++ samples/streams.s
@@ -2,15 +2,23 @@
 
 (load "stdlib/streams.txt")
 
-(defun generate-ints-up-to-3 (n)
-  (if (> n 3)
-    ()
-    (list n (+ n 1))))
+;(defun generate-ints-up-to-3 (n)
+;  (if (> n 3)
+;    ()
+;    (list n (+ n 1))))
 
-(def ints-up-to-3-stream (stream/new generate-ints-up-to-3 1))
+;(def ints-up-to-3-stream (stream/new generate-ints-up-to-3 1))
 
-(let ((s ints-up-to-3-stream))
-  (while (not (stream/empty? s))
-    (print (stream/car s))
-    (set! s (stream/cdr s))))
+;(let ((s ints-up-to-3-stream))
+;  (while (not (stream/empty? s))
+;    (print (stream/car s))
+;    (set! s (stream/cdr s))))
 
+(defun int-generator (n)
+  (list n (+ n 1)))
+
+(def s (stream/new int-generator 1))
+
+(while (not (stream/empty? s))
+  (print (stream/car s))
+  (set! s (stream/cdr s)))
